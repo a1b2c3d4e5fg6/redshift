2801f8d7ca86b3f0eeceb327052ce5c99bb0a21b
Abdullah <mad8zad@gmail.com>
2025-09-15 08:06:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -387,6 +387,9 @@ init_db()
 application = app
 
 if __name__ == '__main__':
+    # Get port from environment variable or use default
+    port = int(os.environ.get('PORT', 5000))
+    
     # In production, Render handles SSL termination
     # We only need to run the app without SSL
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=port)
